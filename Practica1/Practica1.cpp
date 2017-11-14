94769ed52aebd14b740520d117854d3c6f34ffb1
Carlos Díez <cdiezm14@gmail.com>
2017-11-14 21:55:25 +0100

    Añadidos struc de bala y pj. Comienzo a separar codigo en funciones

--- Practica1/Practica1.cpp
+++ Practica1/Practica1.cpp
@@ -4,6 +4,7 @@
 #include <Windows.h>
 #include <stdlib.h>     
 #include <time.h>     
+#include <list>
 
 #define ESC_KEY 27
 #define J_KEY 'j'
@@ -11,21 +12,44 @@
 #define L_KEY 'l'
 #define H_KEY 'h'
 
+//Entities
+struct bala {
+	int x;
+	int dir;
+};
+
+struct pj {
+	int x;
+	int lives;
+};
+
+//meter puntos dentro de pj?????
+
 int main(){
 	//var
 	const unsigned int        size           = 80;
-	unsigned int              x_pos          = 40;
-	int                       left_bullet    = -1;
-	unsigned int              right_bullet   = size + 1;
-	int                       enemy_pos      = -1;
+	pj                        main_pj        = { 40, 3 };
+//	int                       pj_pos         = 40;
+//	int                       left_bullet    = -1;
+//	int                       right_bullet   = size + 1;
+//	int                       enemy_pos      = -1;
 	int                       key	         = 0;
 	bool                      end_game       = false;
 	const unsigned int        points_mush    = 100;
 	int                       mush_pos       = -1;
 	unsigned int              score          = 0;
-	unsigned int              lives          = 3;
+//	unsigned int              lives          = 3;
+	std::list<int>			  mush_list;
+	std::list<int>			  enemy_list;
+	std::list<bala>			  bullet_list;
 
 	srand(time(NULL));
+
+	printf("\n");
+	printf("\n"); 
+	printf("\n");
+	printf("\n");
+	printf("\n");
 
 	while (key != ESC_KEY && !end_game) {
 		//Lectura
@@ -34,36 +58,36 @@ int main(){
 			key = _getch();
 			switch (key) {
 				case J_KEY:
-					if (x_pos > 0) {
-						x_pos--;
-						if (enemy_pos == x_pos) {
-							if (lives > 0) {
-								lives--;
+					if (main_pj.x > 0) {
+						main_pj.x--;
+						if (enemy_pos == main_pj.x) {
+							if (main_pj.lives > 0) {
+								main_pj.lives--;
 								enemy_pos = -1;
 							}
 							else {
 								end_game = true;
 							}
 						}
-						if (x_pos == mush_pos) {
+						if (main_pj.x == mush_pos) {
 							mush_pos = -1;
 							score += points_mush;
 						}
 					}
 					break;
 				case K_KEY:
-					if (x_pos < size - 1) {
-						x_pos++;
-						if (enemy_pos == x_pos) {
-							if (lives > 0) {
-								lives--;
+					if (main_pj.x < size - 1) {
+						main_pj.x++;
+						if (enemy_pos == main_pj.x) {
+							if (main_pj.lives > 0) {
+								main_pj.lives--;
 								enemy_pos = -1;
 							}
 							else {
 								end_game = true;
 							}
 						}
-						if (x_pos == mush_pos) {
+						if (pj_pos == mush_pos) {
 							mush_pos = -1;
 							score += points_mush;
 						}
@@ -72,14 +96,14 @@ int main(){
 				case L_KEY:
 					if (left_bullet < 0) {
 						if (right_bullet == size + 1) {
-							right_bullet = x_pos + 1;
+							right_bullet = main_pj.x + 1;
 						}
 					}
 					break;
 				case H_KEY:
 					if (right_bullet == size + 1) {
 						if (left_bullet == -1) {
-							left_bullet = x_pos - 1;
+							left_bullet = main_pj.x - 1;
 						}
 					}
 					break;
@@ -103,7 +127,7 @@ int main(){
 
 		//Enemy
 		if (enemy_pos != -1) {
-			if (enemy_pos > x_pos) {
+			if (enemy_pos > main_pj.x) {
 				enemy_pos--;
 			} 
 			else {
@@ -124,9 +148,9 @@ int main(){
 			}
 		}
 
-		if (enemy_pos == x_pos) {
-			if (lives > 0) {
-				lives--;
+		if (enemy_pos == main_pj.x) {
+			if (main_pj.lives > 0) {
+				main_pj.lives--;
 				enemy_pos = -1;
 			}
 			else {
@@ -139,8 +163,8 @@ int main(){
 		if (mush_pos == -1) {
 			int mush_rand = rand() % 70 + 1;
 			if (mush_rand == 4) {
-				mush_pos = x_pos;
-				while (mush_pos == x_pos) {
+				mush_pos = main_pj.x;
+				while (mush_pos == main_pj.x) {
 					mush_pos = rand() % size;
 				}
 			}
@@ -150,14 +174,14 @@ int main(){
 		//Pintado
 		if (end_game) {
 			printf("\n\nYOU LOSE!!!!!!!!");
-		} 
+		}
 		else {
 			printf("\r");
-			printf("%i ", lives);
+			printf("%i ", main_pj.lives);
 			for (int i = 0; i < size; i++) {
-				if (i == x_pos) {
+				if (i == main_pj.x) {
 					printf("%c", 'X');
-				} 
+				}
 				else if (i == right_bullet) {
 					printf("%c", '>');
 				}
@@ -180,4 +204,30 @@ int main(){
 	getchar();
     return 0;
 }
+
+void checkEnemyColission(std::list<int> *enemy_list, pj &main_pj) {
+	for (auto it = enemy_list->begin(); it != enemy_list->end();) {
+		if (main_pj.x + 1 == *it || main_pj.x - 1 == *it) {
+			enemy_list->erase(it);
+			main_pj.lives--;
+		}
+		else {
+			it++;
+		}
+	}
+}
+
+void checkMushroomColission(std::list<int> *mush_list, pj &main_pj) {
+	for (auto it = mush_list->begin(); it != mush_list->end();) {
+		if (main_pj.x + 1 == *it || main_pj.x - 1 == *it) {
+			mush_list->erase(it);
+			//main_pj.lives--;
+		}
+		else {
+			it++;
+		}
+	}
+}
+
+
 
